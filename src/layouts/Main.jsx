245dad72bdaea5284a48bd3194ba247205aef1dc
foisal404 <foisalahmmed2@gmail.com>
2025-08-06 19:45:05 +0600

    Main layout create

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { FaPlus } from "react-icons/fa";
+import Home from "../components/Home";
 
 export default function Main() {
   return (
-    <div className="h-screen relative">
-      <button className="bg-blue-500 text-white p-2 rounded absolute bottom-28 left-4">
-        <FaPlus />
-      </button>
-      <div className=" overflow-y-auto">
-        <h2>hello</h2>
-      </div>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
